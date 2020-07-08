8bd58f880ba1158395dbc01ea0e084a69e2d7b6b
kapernikxd <you@example.com>
2020-07-08 11:02:46 +0300

    Базовое подключение socket.io

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ server.listen(3000);
 app.get('/', function(request, respons) {
     respons.sendFile(__dirname + '/index.html');
 });
+
+
+// Массив со всеми подключениями
+connections = [];
+
+// Функция, которая сработает при подключении к странице
+// Считается как новый пользователь
+io.sockets.on('connection', function(socket) {
+    console.log("Успешное соединение");
+    // Добавление нового соединения в массив
+    connections.push(socket);
+
+    // Функция, которая срабатывает при отключении от сервера
+    socket.on('disconnect', function(data) {
+        // Удаления пользователя из массива
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Отключились");
+    });
+
+    // // Функция получающая сообщение от какого-либо пользователя
+    // socket.on('send mess', function(data) {
+    //     // Внутри функции мы передаем событие 'add mess',
+    //     // которое будет вызвано у всех пользователей и у них добавиться новое сообщение
+    //     io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+    // });
+
+});
